7b0dba346274e8d99b42796d039ce634b6452051
ajoriaux <a.joriaux@live.fr>
2023-05-23 11:54:17 +0200

    Test ISBN valide

--- src/test/java/fr/ajoriaux/tdd/ISBNValidatorTest.java
+++ src/test/java/fr/ajoriaux/tdd/ISBNValidatorTest.java
@@ -10,4 +10,10 @@ public class ISBNValidatorTest {
 		assertThrows(NumberFormatException.class, () -> validator.validateISBN("222647272"));
 		assertThrows(NumberFormatException.class, () -> validator.validateISBN("22264727278"));
 	}
+	
+	@Test
+	public void checkValidISBN() {
+		ISBNValidator validator = new ISBNValidator();
+		assertTrue(validator.validateISBN("2714493238"));
+	}
 }
