ded69562bcba8f2184f65eced9954c3e4265924f
ajoriaux <a.joriaux@live.fr>
2023-05-23 12:01:56 +0200

    refactor ISBNValidatorTest

--- src/test/java/fr/ajoriaux/tdd/ISBNValidatorTest.java
+++ src/test/java/fr/ajoriaux/tdd/ISBNValidatorTest.java
@@ -1,19 +1,26 @@
 package fr.ajoriaux.tdd;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ISBNValidatorTest {
+	private ISBNValidator validator;
+	
+	@BeforeEach
+	public void init() {
+		validator = new ISBNValidator();
+	}
+	
 	@Test
 	public void invalidLengthThrowException() {
-		ISBNValidator validator = new ISBNValidator();
 		assertThrows(NumberFormatException.class, () -> validator.validateISBN("222647272"));
 		assertThrows(NumberFormatException.class, () -> validator.validateISBN("22264727278"));
 	}
 	
 	@Test
 	public void checkValidISBN() {
-		ISBNValidator validator = new ISBNValidator();
 		assertTrue(validator.validateISBN("2714493238"));
 	}
 }
