0b5d4ebdbee31ff54a93ef6480dcf407735dad1c
ajoriaux <a.joriaux@live.fr>
2023-05-23 11:48:48 +0200

    Code ISBN longueur

--- src/main/java/fr/ajoriaux/tdd/ISBNValidator.java
+++ src/main/java/fr/ajoriaux/tdd/ISBNValidator.java
@@ -3,5 +3,6 @@ package fr.ajoriaux.tdd;
 public class ISBNValidator {
 
 	public void validateISBN(String isbn) {
+		if (isbn.length() != 10) throw new NumberFormatException("An ISBN should have 10 numbers.");
 	}
 }
