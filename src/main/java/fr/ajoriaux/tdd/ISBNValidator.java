2c402ff5790439bd857d1dc07af2d879b4c26263
ajoriaux <a.joriaux@live.fr>
2023-05-23 11:58:15 +0200

    code ISBN valide

--- src/main/java/fr/ajoriaux/tdd/ISBNValidator.java
+++ src/main/java/fr/ajoriaux/tdd/ISBNValidator.java
@@ -4,6 +4,13 @@ public class ISBNValidator {
 
 	public boolean validateISBN(String isbn) {
 		if (isbn.length() != 10) throw new NumberFormatException("An ISBN should have 10 numbers.");
-		return false;
+		
+		int total = 0;
+		
+		for (int i = 0; i< 10; i++) {
+			total += isbn.charAt(i) * (10-i);
+		}
+		
+		return (total % 11 == 0);
 	}
 }
