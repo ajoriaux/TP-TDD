a58dd6c26a671716c65844893243e5efca3f3598
ajoriaux <a.joriaux@live.fr>
2023-06-26 10:06:01 +0200

    correction test

--- src/test/java/fr/ajoriaux/tdd/BookValidatorTest.java
+++ src/test/java/fr/ajoriaux/tdd/BookValidatorTest.java
@@ -1,7 +1,6 @@
 package fr.ajoriaux.tdd;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -9,10 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookValidatorTest {
+	BookDataService webService;
 	
 	@Test
 	public void webServiceExist() {
-		DataService webService = mock(BookDataService.class);
+		webService = mock(BookDataService.class);
 		verify(webService).getBookData("2714493238");
 	}
 }
