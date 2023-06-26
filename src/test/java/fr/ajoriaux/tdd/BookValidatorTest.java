122a2ef9a0ba567b8be537361564cb7cf166110d
ajoriaux <a.joriaux@live.fr>
2023-06-26 11:22:04 +0200

    correction test

--- src/test/java/fr/ajoriaux/tdd/BookValidatorTest.java
+++ src/test/java/fr/ajoriaux/tdd/BookValidatorTest.java
@@ -1,18 +1,42 @@
 package fr.ajoriaux.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookValidatorTest {
+	BookDataService dbService;
 	BookDataService webService;
+	
+	@BeforeEach
+	public void initMocks() {
+        dbService = mock(BookDataService.class);
+        webService = mock(BookDataService.class);
+        
+        manager = new BookManager();
+        manager.setDatabaseBookDataService(dbService);
+        manager.setWebBookDataService(webService);
+	}
 	
 	@Test
 	public void webServiceExist() {
-		webService = mock(BookDataService.class);
-		verify(webService).getBookData("2714493238");
+		String isbn = "2714493238";
+        dbService = new BookDataService() {
+           @Override
+           public Book getBookData(String isbn) {
+               return new Book(isbn, "Et c'est ainsi que nous vivrons", "Douglas Kennedy", "Belfond", "Broché", true);
+           }
+       };
+
+       when(webService.getBookData(isbn)).thenReturn(new Book(isbn, "Et c'est ainsi que nous vivrons", "Douglas Kennedy", "Belfond", "Broché", true));
+
+       verify(dbService).getBookData(isbn);
 	}
 }
