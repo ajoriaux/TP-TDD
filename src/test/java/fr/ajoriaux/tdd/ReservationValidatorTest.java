d4aa7ce2d491e4672801fbe4179eda0a6c3ab644
ajoriaux <a.joriaux@live.fr>
2023-06-27 15:12:55 +0200

    correction test

--- src/test/java/fr/ajoriaux/tdd/ReservationValidatorTest.java
+++ src/test/java/fr/ajoriaux/tdd/ReservationValidatorTest.java
@@ -32,8 +32,9 @@ class ReservationValidatorTest {
 	/// Ajout d'une réservation 
 	@Test
 	public void addReservationToDatabase() {
+		Book book = new Book("2714493238", "Et c'est ainsi que nous vivrons", "Douglas Kennedy", "Belfond", "Broché", true);
 		Member member = new Member("MEM1", "Henry", "Thierry", new Date(1984, 4, 8), "M");
-		Reservation reservation = new Reservation("1", member, new Date(2023, 5, 26), new Date(2023, 9, 26));
+		Reservation reservation = new Reservation("1", member, book, new Date(2023, 5, 26), new Date(2023, 9, 26));
 		manager.addReservation(reservation);
 		verify(dbService).createReservation(reservation);
 		assertTrue(manager.addReservation(reservation));
