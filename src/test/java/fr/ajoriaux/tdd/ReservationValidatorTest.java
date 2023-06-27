c764d9f93099fdb3af5d25390742d4c0ec042b71
ajoriaux <a.joriaux@live.fr>
2023-06-27 16:31:49 +0200

    test blocage ajout réservation si l'adhérent en a déjà 3

--- src/test/java/fr/ajoriaux/tdd/ReservationValidatorTest.java
+++ src/test/java/fr/ajoriaux/tdd/ReservationValidatorTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -35,6 +38,24 @@ class ReservationValidatorTest {
 		manager.addReservation(reservation);
 		verify(dbService).createReservation(reservation);
 		assertTrue(manager.addReservation(reservation));
+	}
+	
+	@Test
+	public void MemberCannotHaveMoreThanThreeReservations() {
+		Book book = new Book("2714493238", "Et c'est ainsi que nous vivrons", "Douglas Kennedy", "Belfond", "Broché", true);
+		Member member = new Member("MEM1", "Henry", "Thierry", new Date(1984, 4, 8), "M");
+		
+		ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+		reservations.add(new Reservation("1", member, book, new Date(2023, 5, 26), new Date(2023, 9, 26)));
+		reservations.add(new Reservation("2", member, book, new Date(2023, 5, 12), new Date(2023, 9, 12)));
+		reservations.add(new Reservation("3", member, book, new Date(2023, 4, 26), new Date(2023, 8, 26)));
+		Reservation reservation = new Reservation("4", member, book, new Date(2023, 5, 26), new Date(2023, 9, 26));
+		when(dbService.getReservationsByMember(reservation.getMember().getCode())).thenReturn(reservations);
+		
+		manager.addReservation(reservation);
+		verify(dbService).getReservationsByMember(reservation.getMember().getCode());
+		verifyNoInteractions(dbService).createReservation(reservation);
+		assertFalse(manager.addReservation(reservation));
 	}
 	
 	/// Recherche adhérent par code
